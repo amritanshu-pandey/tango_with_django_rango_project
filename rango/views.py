aae9f5a82eade52dfe32f1dfa888f283e90e78b9
Amritanshu <amp.msit@gmail.com>
2014-12-18 21:13:55 +0530

    lots of changes

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
+from rango.models import Category
 
-# Create your views here.
+
+def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories':category_list}
+    return render(request,"rango/index.html",context_dict)
+    #return HttpResponse("Rango says hey there world!<br><a href='/rango/about/'>About</a>")
+
+def about(request):
+    context_dict = {"message":"Here is the about page!!"}
+    #return HttpResponse("Rango says here is the about page!<br><a href='/rango/'>Rango</a>")
+    return render(request, "rango/about.html", context_dict)
+
